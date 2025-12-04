aceb1d3277bb41ece2964e8c4995f13d9605f776
dsguerrero07 <dsguerrero07@ucatolica.edu.co>
2025-12-03 22:07:59 -0500

    Fix Render SSL connection

--- app/database.py
+++ app/database.py
@@ -1,28 +1,21 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 
-# Cargar variables del archivo .env
 load_dotenv()
 
-# Leer la URL de la base de datos desde .env
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
-    pool_pre_ping=True,
-    connect_args={"sslmode": "require"}
+    pool_pre_ping=True
 )
 
-# Sesión (permite ejecutar operaciones en la BD)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Base de modelos
 Base = declarative_base()
 
-# Dependencia para usar DB en endpoints
 def get_db():
     db = SessionLocal()
     try:
